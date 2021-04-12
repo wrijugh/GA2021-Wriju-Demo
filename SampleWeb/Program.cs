bf2aba04bef845401e515f83a1f818c74c332db3
Wriju Ghosh <wriju@hotmail.com>
2021-04-12 23:44:35 +0530

    6

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//Test 5
+//Test 6
 namespace SampleWeb
 {
     public class Program
