6b4645263322cf8ba32e7ee0e25ecfe21b675e4c
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2021-04-12 22:40:45 +0530

    Update Program.cs

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//Test
+//Test 2
 namespace SampleWeb
 {
     public class Program
