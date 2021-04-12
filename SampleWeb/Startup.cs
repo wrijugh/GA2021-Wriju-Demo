102b50dc09efb277fffc803a4469c718f78e5e07
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2021-04-12 22:36:50 +0530

    Update Startup.cs

--- SampleWeb/Startup.cs
+++ SampleWeb/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+//By Wriju 
 namespace SampleWeb
 {
     public class Startup
