fb364d407a68f2222eb06a54b48cda2e2e048346
Wriju - Personal <49710438+wrijugh@users.noreply.github.com>
2021-04-12 22:38:38 +0530

    Test
    Test

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//Test
 namespace SampleWeb
 {
     public class Program
