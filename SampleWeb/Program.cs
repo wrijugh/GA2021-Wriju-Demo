358d16d0efdf6dd489e90c9133e7d56b0a20a35f
Your Name <you@example.com>
2021-04-12 13:12:10 +0000

    emo

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -20,7 +20,7 @@ namespace SampleWeb
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); //
                 });
     }
 }
