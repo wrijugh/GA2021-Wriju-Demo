c7103c74d93631b68f3d52529d77bc30a16e5dfa
Wriju Ghosh <wriju@hotmail.com>
2021-04-14 20:31:55 +0530

    Modu

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//Test 8
+//Test 9
 namespace SampleWeb
 {
     public class Program
