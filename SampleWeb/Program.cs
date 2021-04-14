2936aec3a55b60c15a3a776272250162bf7fd7f0
Wriju Ghosh <wriju@hotmail.com>
2021-04-14 20:24:12 +0530

    AKS Name

--- SampleWeb/Program.cs
+++ SampleWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-//Test 6
+//Test 7
 namespace SampleWeb
 {
     public class Program
